cafe64f5d5eea5b9961cebb55114cc154d6254a7
Space Cowboy <dxb@boardmansgame.com>
2026-01-15 07:59:44 +0000

    DxB fix merge conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -27,10 +27,6 @@ export default defineConfig({
     },
   },
   server: {
-<<<<<<< HEAD
     allowedHosts: ['jmsm412.textreader.boardmansgame.com', 'traceminerals.boardmansgame.com', '.tryhydrogen.dev'],
-=======
-    allowedHosts: ['.tryhydrogen.dev'],
->>>>>>> origin/dev
   },
 });
